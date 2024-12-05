b4c316800ce7d4e4f7af291a8215250cbb2aaa68
unknown <sealkevinboyabad@gmail.com>
2024-12-05 03:39:09 -0800

    Creada la clase Game

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -1,5 +1,8 @@
-package com.factoriaf5.rps.application;
-
 public class Game {
-    
+    public String play(Player player1, Choice choice1, Player player2, Choice choice2) {
+        if (choice1 == choice2) {
+            return "It's a draw!";
+        }
+        return choice1.beats(choice2) ? player1.getName() + " wins!" : player2.getName() + " wins!";
+    }
 }
